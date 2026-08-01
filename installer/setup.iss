1c64e4a09712b3b32745d57b6b99bf56cb2667d6
mustafaelbaz5 <m9stafa05@gmail.com>
2026-08-01 09:38:48 +0300

    feat: update version to 1.0.6 and increment build number to 6

--- installer/setup.iss
+++ installer/setup.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Hiyaza"
-#define MyAppVersion "1.0.5"
+#define MyAppVersion "1.0.6"
 #define MyAppPublisher "Mustafa Elbaz"
 #define MyAppExeName "hiyaza_finder.exe"
 
--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -21,8 +21,8 @@ class AppConfig {
     'APP_NAME',
     defaultValue: 'HiyazaFinder Dev',
   );
-  static const String appVersion = '1.0.5';
-  static const String buildNumber = '5';
+  static const String appVersion = '1.0.6';
+  static const String buildNumber = '6';
 
   // Developer Info
   static const String developerName = 'Mustafa Elbaz';
