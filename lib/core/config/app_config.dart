62b59e83e119390b51900396c77db8999d67a5c9
mustafaelbaz5 <m9stafa05@gmail.com>
2026-08-04 08:28:14 +0300

    build new version 1.0.9

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -21,8 +21,8 @@ class AppConfig {
     'APP_NAME',
     defaultValue: 'HiyazaFinder Dev',
   );
-  static const String appVersion = '1.0.8';
-  static const String buildNumber = '8';
+  static const String appVersion = '1.0.9';
+  static const String buildNumber = '9';
 
   // Developer Info
   static const String developerName = 'Mustafa Elbaz';
