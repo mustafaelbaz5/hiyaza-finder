5204188a555e00ea227ab76f5aae67dfaed27110
mustafaelbaz5 <m9stafa05@gmail.com>
2026-08-27 09:25:04 +0300

    feat: register JazlaSearchService and JazlaExportService in Jazla module

--- lib/core/di/modules/jazla_module.dart
+++ lib/core/di/modules/jazla_module.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
 
+import '../../../features/jazla/data/local/jazla_export_service.dart';
+import '../../../features/jazla/data/local/jazla_search_service.dart';
 import '../../../features/jazla/data/local/jazla_store.dart';
 import '../../../features/jazla/data/repo/jazla_repo.dart';
 import '../../../features/jazla/data/repo/jazla_repo_impl.dart';
@@ -12,4 +14,6 @@ void registerJazlaModule(final GetIt getIt) {
   getIt.registerLazySingleton<JazlaRepo>(
     () => JazlaRepoImpl(store: getIt<JazlaStore>()),
   );
+  getIt.registerLazySingleton(() => const JazlaSearchService());
+  getIt.registerLazySingleton(() => const JazlaExportService());
 }
